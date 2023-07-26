2ab94c2b201bbf39eef42e254cae9702feb63aff
Sonam Dutta <78217309+SonamGit89@users.noreply.github.com>
2023-07-26 09:39:29 +0530

    Delete activity1.py

--- Python/activity1.py
+++
@@ -1,4 +0,0 @@
-name = input( "What is your name: " )
-age = int( input( "How old are you: " ) )
-year = str ((2023 - age) + 100) # using typecast made it string to get it printed in enxt line
-print( name + " will be 100 years old in the year " + year )
